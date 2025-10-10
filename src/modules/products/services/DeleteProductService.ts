598e90cca2b0c51a1acb7baf9d8b3f10e2fb42ea
wagner1067 <barra-wag@hotmail.com>
2025-10-09 21:36:56 -0300

    feat: delete product service

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,17 @@
+import AppError from "@shared/errors/AppError";
+import { productsRepositories } from "../database/repositories/ProductsRepositories";
+
+interface IDeleteProduct {
+  id: string;
+}
+
+export default class DeleteProductService {
+  async execute({ id }: IDeleteProduct): Promise<void> {
+    const product = await productsRepositories.findById(id);
+    if (!product) {
+      throw new AppError("Produto não encontrado", 404);
+    }
+
+    await productsRepositories.remove(product);
+  }
+}
