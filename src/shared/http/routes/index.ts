e62833267ac7ee5cfd4d694ee27d9f1ef439b4ca
wagner1067 <barra-wag@hotmail.com>
2025-11-02 11:20:33 -0300

    feat: routes uploads directory

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,7 +2,8 @@ import productsRoutes from "@modules/products/routes/ProductRoutes";
 import avatarRouter from "@modules/users/routes/AvatarRoutes";
 import sessionRouter from "@modules/users/routes/SessionRoutes";
 import usersRouter from "@modules/users/routes/UserRoutes";
-import { Router } from "express";
+import express, { Router } from "express";
+import uploadConfig from "@config/upload";
 
 const routes = Router();
 
@@ -14,5 +15,6 @@ routes.use("/products", productsRoutes);
 routes.use("/users", usersRouter);
 routes.use("/sessions", sessionRouter);
 routes.use("/avatars", avatarRouter);
+routes.use("/files", express.static(uploadConfig.directory));
 
 export default routes;
