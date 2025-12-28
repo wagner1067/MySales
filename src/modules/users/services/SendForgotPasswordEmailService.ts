253474616e4bcfc5ce8e9e398d4abe6c9264ba8f
wagner1067 <barra-wag@hotmail.com>
2025-12-28 15:44:33 -0300

    feat: email config and send

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,7 @@
 import AppError from "@shared/errors/AppError";
 import { usersRepositories } from "../database/repositories/UsersRepositories";
 import { userTokensRepositories } from "../database/repositories/UserTokensRepositories";
+import { sendEmail } from "@config/email";
 
 interface IForgotPassword {
   email: string;
@@ -16,6 +17,20 @@ export default class SendForgotPasswordEmailService {
 
     const token = await userTokensRepositories.generate(user.id);
 
-    console.log(token);
+    sendEmail({
+      to: email,
+      subject: "My Sales Recovery Password",
+      body: `
+        <div style="font-family: Arial, sans-serif; padding: 20px; color: #333; text-align: center; border: 2px solid #041d40; border-radius: 10px; margin: auto; width: 60%;">
+          <h1 style="color: #041d40;">Password Reset Verification Code</h1>
+          <h3 style="color: #041d40;">Dear ${user.name},</h3>
+          <p style="font-size: 16px; color: #333;">Recover your password with this token:</p>
+          <p>
+            <strong style="border: 2px dashed #041d40; padding: 10px; border-radius: 5px; font-size: 16px; color: #041d40;">${token?.token}</strong>
+          </p>
+          <p style="margin-top: 20px;">Best regards,<br><span style="font-weight: bold; color: #041d40;">My Sales Staff</span></p>
+        </div>
+      `,
+    });
   }
 }
