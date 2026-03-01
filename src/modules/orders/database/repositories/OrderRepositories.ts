db579d865773c0ec75e6b9a64a35529728b8a992
wagner1067 <barra-wag@hotmail.com>
2026-03-01 16:55:58 -0300

    feat: create order repositories

---
+++ src/modules/orders/database/repositories/OrderRepositories.ts
@@ -0,0 +1,29 @@
+import { Customers } from "@modules/customers/database/entities/Customers";
+import { AppDataSource } from "@shared/typeorm/data-source";
+import { Order } from "../entities/Order";
+import { OrderProducts } from "../entities/OrderProducts";
+
+interface ICreateOrder {
+  customer: Customers;
+  products: OrderProducts[];
+}
+
+export const orderRepositories = AppDataSource.getRepository(Order).extend({
+  async findById(id: number): Promise<Order | null> {
+    const order = await this.findOne({
+      where: { id },
+      relations: ["order_products", "customer"],
+    });
+
+    return order;
+  },
+
+  async createOrder({ customer, products }: ICreateOrder): Promise<Order> {
+    const order = this.create({
+      customer,
+      order_products: products,
+    });
+    await this.save(order);
+    return order;
+  },
+});
