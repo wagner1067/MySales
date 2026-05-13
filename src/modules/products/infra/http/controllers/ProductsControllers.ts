22c7369093bfb33c543f1f7e81cffe97c804bfdb
wagner1067 <barra-wag@hotmail.com>
2026-05-12 21:53:13 -0300

    Fix: Correct Codigo

--- src/modules/products/infra/http/controllers/ProductsControllers.ts
+++ src/modules/products/infra/http/controllers/ProductsControllers.ts
@@ -1,53 +1,72 @@
 import { Request, Response } from "express";
-import DeleteProductService from "@modules/products/services/DeleteProductService";
-import ListProductService from "@modules/products/services/ListProductService";
+
 import ShowProductService from "@modules/products/services/ShowProductService";
+
 import UpdateProductService from "@modules/products/services/UpdateProductService";
+import DeleteProductService from "@modules/products/services/DeleteProductService";
 import { container } from "tsyringe";
+import ListProductService from "@modules/products/services/ListProductService";
 import { CreateProductService } from "@modules/products/services/CreateProductService";
 
-export default class ProductsControllers {
-  async index(request: Request, response: Response): Promise<Response> {
+export default class ProductsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { page, skip, take } = request.query;
     const listProductsService = container.resolve(ListProductService);
-    const products = await listProductsService.execute();
+    const products = await listProductsService.execute({
+      page: Number(page),
+      skip: Number(skip),
+      take: Number(take),
+    });
+
     return response.json(products);
   }
 
-  async show(request: Request, response: Response): Promise<Response> {
+  public async show(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
     const showProductService = container.resolve(ShowProductService);
-    const product = await showProductService.execute({ id });
-    return response.json(product);
+
+    const products = await showProductService.execute({ id });
+
+    return response.json(products);
   }
 
-  async create(request: Request, response: Response): Promise<Response> {
+  public async create(request: Request, response: Response): Promise<Response> {
     const { name, price, quantity } = request.body;
+
     const createProductService = container.resolve(CreateProductService);
+
     const product = await createProductService.execute({
       name,
       price,
       quantity,
     });
+
     return response.json(product);
   }
 
-  async update(request: Request, response: Response): Promise<Response> {
+  public async update(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
     const { name, price, quantity } = request.body;
+
     const updateProductService = container.resolve(UpdateProductService);
+
     const product = await updateProductService.execute({
       id,
       name,
       price,
       quantity,
     });
+
     return response.json(product);
   }
 
-  async delete(request: Request, response: Response): Promise<Response> {
+  public async delete(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
+
     const deleteProductService = container.resolve(DeleteProductService);
+
     await deleteProductService.execute({ id });
+
     return response.status(204).send([]);
   }
 }
